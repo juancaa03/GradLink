3227ec0ce9ac2f4da8ba92b0b61aa74fbeb261f8
Juan Carlos Medinilla <juancarlos.medinilla@estudiants.urv.cat>
2025-04-09 08:38:19 +0200

    correccion vulnerabilidad broken acces control

--- gradlink-frontend/src/index.jsx
+++ gradlink-frontend/src/index.jsx
@@ -1,27 +1,32 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import ProtectedRoute from "./ProtectedRoute.jsx";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
+// Puedes mover esto a un `routes.js` más adelante si crece
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute allowedRoles={["student", "admin", "dev"]}>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      
-      {/* Ruta protegida */}
       <Route
         path="/dashboard"
         element={
-          <ProtectedRoute>
+          <ProtectedRoute allowedRoles={["admin"]}>
             <Dashboard />
           </ProtectedRoute>
         }
       />
-
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
